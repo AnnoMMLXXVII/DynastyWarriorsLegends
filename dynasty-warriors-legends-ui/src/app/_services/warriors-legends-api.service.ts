e994c243d239eda1e5e15e006cf19f7d79145a5d
Tess Tickle <annommlxxvii@TessTickle.local>
2024-09-28 03:19:19 -0400

    updated servicesAPI from localhost to deployed url

--- dynasty-warriors-legends-ui/src/app/_services/warriors-legends-api.service.ts
+++ dynasty-warriors-legends-ui/src/app/_services/warriors-legends-api.service.ts
@@ -1,18 +1,19 @@
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { WarriorsApiEndpoints } from '../_shared/warriors-api-endpoints';
+import { WARRIORS_SERVICES_API_HOST, WarriorsApiEndpoints } from '../_shared/warriors-api-endpoints';
 @Injectable({
   providedIn: 'root',
 
 })
 export class WarriorsLegendsAPIService {
 
-  private baseURL = "http://localhost:8080/" + WarriorsApiEndpoints.BASE_CONTEXT;
+  private baseURL = "https://" + WARRIORS_SERVICES_API_HOST + "/" + WarriorsApiEndpoints.BASE_CONTEXT;
 
   constructor(private http: HttpClient) { }
 
   public httpGet(resource: string) {
     let url = this.baseURL + resource;
+    console.log(url);
     return this.http.get(url + "/");
   }
 
